0af6c0f02fe2ac8e1114495356e81ab43c17d32f
David Gageot <david@gageot.net>
2019-03-19 18:23:46 +0100

    Initial version
    Signed-off-by: David Gageot <david@gageot.net>

---
+++ .demoit/.bash_history
@@ -0,0 +1,11 @@
+go run main.go
+docker build . -t hello:v1
+docker run --rm -it hello:v1
+vi pod.yaml
+kubectl apply -f pod.yaml
+kubectl logs -f hello
+kctx gke_dga-demo_europe-west1-b_bbl
+kctx docker-for-desktop
+kubectl get pods
+skaffold run --tail
+skaffold init -f skaffold.yaml
---
+++ hexagons/gaming-proxy/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/game", game)
+
+	log.Println("Listening on port 8888")
+	http.ListenAndServe(":8888", nil)
+}
+
+func game(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.RequestURI)
+
+	req, err := http.NewRequest("GET", "http://game:8888/game", nil)
+	if err != nil {
+		http.Error(w, "Unable to create request", 500)
+		return
+	}
+
+	copyHeader(req.Header, r.Header)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		http.Error(w, "unable to connect to upstream", 500)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		http.Error(w, resp.Status, resp.StatusCode)
+		return
+	}
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Unable to read hexagons", 500)
+		return
+	}
+
+	copyHeader(w.Header(), resp.Header)
+	w.Write(buf)
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
