8ce75ed22566a1e3fd6e0208a8f5d6bd19544dec
David Gageot <david@gageot.net>
2019-03-28 13:23:26 +0100

    Proxy now retries queries
    Signed-off-by: David Gageot <david@gageot.net>

--- gcb/gaming-proxy/main.go
+++ gcb/gaming-proxy/main.go
@@ -4,78 +4,70 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
+const maxRetry = 30
+
 func main() {
-	http.HandleFunc("/game", game)
-	http.HandleFunc("/title", title)
+	http.HandleFunc("/game", proxy("http://game:8888/game"))
+	http.HandleFunc("/title", proxy("http://title:8888/title"))
 
 	log.Println("Listening on port 8888")
 	http.ListenAndServe(":8888", nil)
 }
 
-func game(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.RequestURI)
-
-	req, err := http.NewRequest("GET", "http://game:8888/game", nil)
-	if err != nil {
-		http.Error(w, "Unable to create request", 500)
-		return
+func proxy(url string) func(http.ResponseWriter, *http.Request) {
+	client := &http.Client{
+		Timeout: 1 * time.Second,
 	}
 
-	copyHeader(req.Header, r.Header)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		http.Error(w, "unable to connect to upstream", 500)
-		return
+	return func(w http.ResponseWriter, r *http.Request) {
+		for retry := 1; retry <= maxRetry; retry++ {
+			log.Println(r.RequestURI)
+
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				http.Error(w, "Unable to create request", 500)
+				return
+			}
+
+			copyHeader(req.Header, r.Header)
+			resp, err := client.Do(req)
+			if err != nil {
+				if retry == maxRetry {
+					http.Error(w, "unable to connect to upstream", 500)
+					return
+				}
+
+				log.Println("Error:", r.RequestURI, err, "Retrying in 1s")
+				time.Sleep(500 * time.Millisecond)
+				continue
+			}
+			defer resp.Body.Close() // TODO fix this
+
+			if resp.StatusCode >= 400 {
+				if retry == maxRetry {
+					http.Error(w, resp.Status, resp.StatusCode)
+					return
+				}
+
+				log.Println("Error:", r.RequestURI, err, "Retrying in 1s")
+				time.Sleep(500 * time.Millisecond)
+				continue
+			}
+
+			buf, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				http.Error(w, "Unable to read hexagons", 500)
+				return
+			}
+
+			copyHeader(w.Header(), resp.Header)
+			w.Write(buf)
+			return
+		}
 	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode >= 400 {
-		http.Error(w, resp.Status, resp.StatusCode)
-		return
-	}
-
-	buf, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		http.Error(w, "Unable to read hexagons", 500)
-		return
-	}
-
-	copyHeader(w.Header(), resp.Header)
-	w.Write(buf)
-}
-
-func title(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.RequestURI)
-
-	req, err := http.NewRequest("GET", "http://title:8888/title", nil)
-	if err != nil {
-		http.Error(w, "Unable to create request", 500)
-		return
-	}
-
-	copyHeader(req.Header, r.Header)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		http.Error(w, "unable to connect to upstream", 500)
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode >= 400 {
-		http.Error(w, resp.Status, resp.StatusCode)
-		return
-	}
-
-	buf, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		http.Error(w, "Unable to read hexagons", 500)
-		return
-	}
-
-	copyHeader(w.Header(), resp.Header)
-	w.Write(buf)
 }
 
 func copyHeader(dst, src http.Header) {
