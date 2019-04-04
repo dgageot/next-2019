a293f5b3c31629c951fd3701b5efed71a8b5c421
David Gageot <david@gageot.net>
2019-04-04 15:42:04 +0200

    Better reset
    Signed-off-by: David Gageot <david@gageot.net>

--- reset.sh
+++ reset.sh
@@ -9,6 +9,27 @@ if [[ "$(git describe --tags --always)" != "v1" ]]; then
     exit 1
 fi
 
+echo "Revoke additional accounts"
+TO_REVOKE=$(gcloud auth list --format="value(account)" | grep -v 'dgageot@gmail.com' || true)
+if [ ! -z "${TO_REVOKE}" ]; then
+    gcloud auth revoke ${TO_DELETE}
+fi
+
+echo "Reset GCP project"
+if [ "$(gcloud config get-value project)" != "next19" ]; then
+  gcloud config set project next19
+fi
+
+echo "Get GKE cluster credentials"
+gcloud container clusters get-credentials demo --zone us-west2-a --project next19
+
+echo "Delete every k8s resource"
+kctx docker-for-desktop
+TO_DELETE=$(kubectl get services,deployments.apps,pods -o name | grep -v 'service/kubernetes' || true)
+if [ ! -z "${TO_DELETE}" ]; then
+    kubectl delete ${TO_DELETE}
+fi
+
 cd repeat
 go build -o /dev/null main.go
 docker build . -t hello:v1
@@ -24,13 +45,11 @@ cd ..
 cd jib
 ./mvnw package
 ./mvnw compile jib:build -Dimage=gcr.io/dga-demo/hello-jib
-kctx docker-for-desktop
 skaffold build
 cd ..
 
 cd hello
 rm -f skaffold.yaml
-kctx docker-for-desktop
 skaffold build -f <(skaffold init -f - --force)
 skaffold delete -f <(skaffold init -f - --force)
 cd ..
@@ -45,7 +64,6 @@ skaffold delete
 cd ..
 
 cd gcb
-kctx docker-for-desktop
 skaffold build
 skaffold delete
 skaffold build -p cloud-build
